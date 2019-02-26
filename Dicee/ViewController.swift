77e8d01e63fa1de4052e8b52723923675dce6698
Deeter <deeter.cesler@gmail.com>
2019-02-26 15:40:52 -0700

    basic layout

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Deeter Cesler on 1/14/19.
+//  Copyright © 2019 Deeter Cesler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
